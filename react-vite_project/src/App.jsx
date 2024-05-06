2ca9372b7cda3e82d55b13ad511ff697ea074e19
sp960 <sanket.prajapati0906@gmail.com>
2024-05-06 12:52:27 +0530

    ReactJs Started

---
+++ react-vite_project/src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./Hello"
+
+function App() {
+
+  return (
+    <>
+    <h1>react with vite </h1>
+    <Hello />
+    </>
+  )
+}
+
+export default App
---
+++ react-vite_project/src/Hello.jsx
@@ -0,0 +1,7 @@
+const Hello =  () => {
+            return(
+                        <h1>This is Vite Component.</h1>
+            )
+}
+
+export default Hello
